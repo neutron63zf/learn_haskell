ee225660aaf7f47b4e404bb668d94701c809cca5
Nkowne63 <neutron63zf@gmail.com>
2018-03-19 15:43:13 +0900

    proceed -> 2.8.2

--- hellohs/13loop.hs
+++ hellohs/13loop.hs
@@ -1,9 +1,30 @@
-import Control.Monad (when)
+import Control.Monad (when,forM_)
 
-main = fizzbuzz2
+main = fbz4
 
 -- alias for print
 p = print
+
+fbz4 = do
+    forM_ [1..20] $ \i -> do
+        putStrLn $ fizzbuzz i
+        where
+            fizzbuzz n
+                | n `mod` 15    == 0    = "FizzBuzz"
+                | n `mod` 3     == 0    = "Fizz"
+                | n `mod` 5     == 0    = "Buzz"
+                | otherwise             = show n            
+
+fbz3 = foldr f (return ()) $ map fizzbuzz [1..20]
+    where
+        fizzbuzz n
+            | n `mod` 15    == 0    = "FizzBuzz"
+            | n `mod` 3     == 0    = "Fizz"
+            | n `mod` 5     == 0    = "Buzz"
+            | otherwise             = show n
+        f str act = do
+            putStrLn str
+            act
 
 fizzbuzz2 = loop 0
     where
